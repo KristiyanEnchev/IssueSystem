b7b925a0e380302e8eb67f6b1528ef4298b9c6f3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-03-03 13:55:48 +0200

    More Libraries

---
+++ src/IssueSystem.Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿namespace IssueSystem.Services
+{
+    using Microsoft.EntityFrameworkCore;
+    using AutoMapper;
+
+    using IssueSystem.Data;
+
+    public class BaseService<TEntity>
+        where TEntity : class
+    {
+        protected BaseService(IssueSystemDbContext data, IMapper mapper)
+        {
+            this.Data = data;
+            this.Mapper = mapper;
+        }
+
+        protected IssueSystemDbContext Data { get; }
+
+        protected IMapper Mapper { get; }
+
+        protected IQueryable<TEntity> All() => this.Data.Set<TEntity>();
+
+        protected IQueryable<TEntity> AllAsNoTracking() => this.All().AsNoTracking();
+    }
+}
---
+++ src/IssueSystem.Services/Constants/MessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace IssueSystem.Services.Constants
+{
+    public class MessageConstants
+    {
+        public const string ErrorMessage = "Error";
+        public const string SuccessMessage = "Success";
+        public const string WarningMessage = "Warning";
+    }
+}
