269d1490533665dfefb7384c934e0ea6229fb0d2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-04-11 23:23:06 +0300

    Implement logic for reports and create views

--- src/IssueSystem.Web/IssueSystem/Controllers/TicketController.cs
+++ src/IssueSystem.Web/IssueSystem/Controllers/TicketController.cs
@@ -20,5 +20,26 @@
 
             return View(model);
         }
+
+        public async Task<IActionResult> DailyReport()
+        {
+            var model = await _ticketService.GetDailyTicketsReport(this.User.GetId());
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> WeeklyReport()
+        {
+            var model = await _ticketService.GetWeeklyTicketsReport(this.User.GetId());
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> YearlyReport()
+        {
+            var model = await _ticketService.GetYearlyTicketsReport(this.User.GetId());
+
+            return View(model);
+        }
     }
 }
